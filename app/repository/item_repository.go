dc4c674c141a6fd1bc8f115620089e19e61c6a2a
Takumi <t-nakayama-1w1@eagle.sophia.ac.jp>
2022-11-10 10:41:08 +0900

    add content type optitional filter

--- app/repository/item_repository.go
+++ app/repository/item_repository.go
@@ -1,12 +1,15 @@
 package repository
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/12ev09/info-demo-backend/app/domain"
 	"github.com/jmoiron/sqlx"
 )
 
 type ItemRepository interface {
-	GetItems() ([]domain.Item, error)
+	GetItems(contentType int) ([]domain.Item, error)
 	PostItem(domain.Item) error
 }
 
@@ -18,10 +21,26 @@ func NewItemRepository(db *sqlx.DB) ItemRepository {
 	return &itemRepository{db: db}
 }
 
-func (i *itemRepository) GetItems() ([]domain.Item, error) {
+func (i *itemRepository) GetItems(contentType int) ([]domain.Item, error) {
 	var items []domain.Item
-	if err := i.db.Select(&items, "SELECT id, title, isbn, publisher_name,sales_date,content_type FROM items"); err != nil {
+
+	args := []string{}
+
+	if contentType != 0 {
+		args = append(args, fmt.Sprintf("where content_type=%d", contentType))
+	}
+
+	rows, err := i.db.Queryx(fmt.Sprintf("SELECT * from items %s", strings.Join(args, " ")))
+	if err != nil {
 		return nil, err
+	}
+
+	for rows.Next() {
+		item := domain.Item{}
+		if err := rows.StructScan(&item); err != nil {
+			return nil, err
+		}
+		items = append(items, item)
 	}
 
 	return items, nil
